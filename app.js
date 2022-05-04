c93f28de428c6cd1128d5f0a796c3c00ddb857e3
vedantk2040 <phy@vedant.localdomain>
2022-05-05 02:19:19 +0530

    added ejs templates

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
-
+app.use('view engine', 'ejs');
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 	var today = new Date();
 	var currentDay = today.getDay();
-	
+	var day = "";
 	if (currentDay === 6 || currentDay === 0) {
-		res.sendFile(__dirname + '/weekend.html');
+		day = "Weekend";
 	} else {
-		res.sendFile(__dirname + '/weekday.html');
+		day = "Weekday";
 	}
+	res.render("list", {kindaDay: day});
 });
 
 app.listen(3000, function() {
